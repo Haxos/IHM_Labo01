0221f7f8dd2ebbb35867eb9ba3a4d39a23de19be
Haxos <a.mayocartes@protonmail.com>
2020-10-20 12:18:32 +0200

    base post-it component

---
+++ src/components/PostIt.vue
@@ -0,0 +1,22 @@
+<<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 200,
+    },
+    height: {
+      type: Number,
+      default: 200,
+    },
+    color: {
+      type: String,
+      default: "LimeGreen",
+    },
+  },
+};
+</script>
