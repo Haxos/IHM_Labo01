221166f8e6cb52d500afa82ccc8685c6ce3b51eb
Haxos <a.mayocartes@protonmail.com>
2020-10-28 15:22:28 +0100

    added some elements in postit

--- src/components/PostIt.vue
+++ src/components/PostIt.vue
@@ -17,6 +17,28 @@ export default {
       type: String,
       default: "LimeGreen",
     },
+    title: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
   },
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      z: 0, // the lower, the further on the back it is
+    }
+  },
+  computed: {
+    fontColor() {
+      // TODO: get opposiite saturation & value from color. 
+      // Color between white and black
+      return "White"
+    }
+  }
 };
 </script>
