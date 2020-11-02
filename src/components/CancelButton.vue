f75d7b2f103e8d1047d655c86425be5839d7eab5
uxorialrook <234463f591c>
2020-11-02 11:20:52 +0100

    First iteration of the CreatePostIt vue

---
+++ src/components/CancelButton.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <div class="rounded-zone cancel-button" @click="onClick">
+        <img class="icon-zone" src="../assets/cancel.png">
+    </div>
+</div>
+</template>
+
+<script>
+
+import "../assets/main.scss"
+export default {
+  props: {
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+      onClick() {
+          this.$emit("canceled");
+      }
+  }
+};
+</script>
---
+++ src/components/CreatePostIt.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <div class="create-postit-container">
+        <form>
+            <input type="text" id="title" name="title">
+            <label for="title">Titre</label>
+        </form>
+        <CancelButton @canceled="onCancel" />
+        <ValidateButton/>
+    </div>
+</div>
+</template>
+
+<script>
+
+import "../assets/main.scss"
+import CancelButton from "./CancelButton"
+import ValidateButton from "./ValidateButton"
+export default {
+    components: {
+        CancelButton,
+        ValidateButton
+    },
+    props: {
+        
+    },
+    methods: {
+        onCancel() {
+            this.$emit("canceled");
+        }
+    },
+    data() {
+    return {
+
+    }
+    },
+    computed: {
+
+    }
+};
+</script>
